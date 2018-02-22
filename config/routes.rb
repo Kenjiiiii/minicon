2cfd12fd342fc983efb5f3accf0001291c273013
Kenji Azaki <35862588+Kenjiiiii@users.noreply.github.com>
2018-02-22 06:56:16 +0000

    Create event controller, model and some views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,26 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'events/index'
+  end
+
+  namespace :admin do
+    get 'events/show'
+  end
+
+  namespace :admin do
+    get 'events/new'
+  end
+
+  namespace :admin do
+    get 'events/edit'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'controller_name#action_name'
   root 'application#hello'
   
   namespace :admin do
     resources :event_categories
+    resources :events
   end
 end
